bde043c17918f694b3e1ff00550e4ec6f78841b3
unknown <luisandypp@gmail.com>
2024-11-25 08:41:53 -0400

    add 2

--- hola_mundo.py
+++ hola_mundo.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     nombre = os.getenv("USERNAME")
-    print(f"¡Hola, {nombre} desde GitHub! UwU")
+    print(f"¡Hola, {nombre} desde GitHub! UwU xd")
 
 if __name__ == "__main__":
     main()
